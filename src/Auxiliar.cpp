cd531523abb99e9c16604c398e5a427fc24a6b36
Sebastián Godínez <sgodinez@nursoft.cl>
2020-06-25 19:46:46 -0400

    change the aspect of the solution

--- src/Auxiliar.cpp
+++ src/Auxiliar.cpp
@@ -9,14 +9,18 @@ using namespace std;
 void showSolution(vector<vector<int>> solution) {
     cout << "Schedule: \n";
     // first show the name of the team
+    cout << "R\\T|";
     for(long unsigned int i = 0; i < solution[0].size(); i++)
-        cout << "T" << (1+i) << " ";
+        cout << "T" << i+1 << ((i+1 >= 10) ? " " : "  ");
     cout << endl;
 
     // print the matrix values
+    int round = 0;
     for (vector<int> &line : solution) {
+        ++round;
+        cout << " " << round << ((round >= 10) ? "" : " ") <<"|";
         for(int &value : line)
-            cout << ((value >= 0) ? " " : "") << value << " ";
+            cout << (value >= 0 ? " " : "") << value << (abs(value) < 10 ? "  " : " ");
         cout << endl;   
     }
 }
