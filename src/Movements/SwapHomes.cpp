92e7a7ee223f3bc87f2babb9f6c8cd3938ffb307
Sebastián Godínez <sgodinez@nursoft.cl>
2020-06-28 03:21:46 -0400

    add tabusearch for just swaphomes

--- src/Movements/SwapHomes.cpp
+++ src/Movements/SwapHomes.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <math.h>
+#include "../Entities/Tournament.cpp"
+#include "../Objective.cpp"
 
 using namespace std;
 
@@ -24,4 +26,57 @@ vector<vector<int>> SwapHomes(vector<vector<int>> scheduling, int teamA, int tea
         }
 
     return scheduling;
+}
+
+bool InTabuList(vector<TabuSwapHomes> tabuList, int teamA, int teamB){
+  for(TabuSwapHomes pair : tabuList){
+    bool cond1 = (pair.teamA == teamA && pair.teamB == teamB);
+    bool cond2 = (pair.teamA == teamB && pair.teamB == teamA);
+    if(cond1 || cond2)
+      return true;
+  }
+  return false;
+}
+
+TSTournament BestSwapHomes(vector<vector<int>> distances, TSTournament scheduling, vector<TabuSwapHomes> &tabuList){
+  unsigned int totalTeams = scheduling.getSchedule()[0].size();
+  unsigned long int bestResult = scheduling.getDistance();
+  unsigned long int auxResult;
+  TabuSwapHomes tempValues;
+  vector<vector<int>> tempScheduling;
+
+  for (unsigned int auxTeamA = 0; auxTeamA < totalTeams; auxTeamA++)
+    for (unsigned int auxTeamB = auxTeamA; auxTeamB < totalTeams; auxTeamB++)
+      if(!InTabuList(tabuList, auxTeamA, auxTeamB)){
+        // get scheduling from movement
+        tempScheduling = SwapHomes(scheduling.getSchedule(), auxTeamA, auxTeamB);
+        auxResult = Distance(distances, tempScheduling);
+        // compare
+        if(auxResult < scheduling.getDistance()){
+          scheduling.setDistance(auxResult);
+          scheduling.setSchedule(tempScheduling);
+          tempValues.teamA = auxTeamA;
+          tempValues.teamB = auxTeamB;
+        }
+      }
+
+  if (bestResult != scheduling.getDistance()){ // add the result to the list
+    if (tabuList.size() == 2){
+      tabuList.erase(tabuList.begin());
+    }
+    tabuList.push_back(tempValues);
+  }
+  
+  return scheduling;
+}
+
+vector<vector<int>> TabuSearchSwapHomes(vector<vector<int>> distances, TSTournament scheduling, int DEBUG=0){
+  vector<TabuSwapHomes> tabuList;
+
+  for(int i = 0; i < 100; i++){
+    scheduling = BestSwapHomes(distances, scheduling, tabuList);
+    if(DEBUG) scheduling.print();
+  }
+
+  return scheduling.getSchedule();
 }
