6b33c659d2befb9853c24b8a33dbac7400ec2bc5
Sebastián Godínez <sgodinez@nursoft.cl>
2020-06-28 01:13:38 -0400

    add the penalty to the result

--- src/Objective.cpp
+++ src/Objective.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
+
+float SmothFunction(int penalty){
+    return  1 + (sqrt(penalty)*log(penalty))/2;
+}
+
+float DistancePenalty(int distance, int penalty){
+    float f = SmothFunction(penalty);
+    float weigth = 100*penalty;
+
+    float powDistance = pow(distance, 2);
+    float powPenalty = pow(weigth*f, 2);
+
+    return sqrt(powDistance + powPenalty);
+}
 
 unsigned int Distance(vector<vector<int>> solution, vector<vector<int>> distances, int DEBUG) {
     unsigned int distance = 0;
@@ -14,8 +29,8 @@ unsigned int Distance(vector<vector<int>> solution, vector<vector<int>> distance
         trip = 0; // sum if a team has a local/visit trip > 3
         localDistance = 0;
         actualCity = actualTeam; // start point of the traveling
-        for (vector<int> round : solution) {
-            int oponent = round[actualTeam];
+        for (long unsigned int round = 0; round < solution.size(); round++) {
+            int oponent = solution[round][actualTeam];
             int oponentPost = abs(oponent) - 1;
             // if (actualCity == actualTeam && oponent < 0) don't sum
             if(actualCity != actualTeam && oponent < 0) { // distance from point to house
@@ -30,6 +45,10 @@ unsigned int Distance(vector<vector<int>> solution, vector<vector<int>> distance
             if (oponent > 0) trip++;
             else trip--;
             if (trip > 3 || trip < -3) penalty++;
+            // penalty: a team cant play vs the same team in a row
+            if(round < (solution.size()-1))
+                if(abs(oponent) == abs(solution[round+1][actualTeam]))
+                    penalty++;
         }
         if (actualCity != actualTeam) localDistance += distances[actualCity][actualTeam]; // if the team dont finish in home
 
@@ -39,7 +58,12 @@ unsigned int Distance(vector<vector<int>> solution, vector<vector<int>> distance
     }
 
     if(DEBUG) cout << "[DEBUG] cantidad de faltas: " << penalty << endl;
-    if(DEBUG) cout << "Distancia total: " << distance << endl;
+    if(DEBUG) cout << "[DEBUG] Distancia total: " << distance << endl;
+
+    if(penalty != 0){
+        distance = DistancePenalty(distance, penalty);
+        if(DEBUG) cout << "[DEBUG] Distancia con penalty: " << distance << endl;
+    }
 
     return distance;
 }
